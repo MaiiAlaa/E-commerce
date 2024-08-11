3a35df226874ac3cf88d8b65788c9d0bf9b978e8
kareem <kareemismail259@gmail.com>
2024-08-11 16:10:54 +0300

    Updated Sign-Up validations

--- src/main/java/org/example/e_commerce/dto/dtoRequest/SignUpRequestDTO.java
+++ src/main/java/org/example/e_commerce/dto/dtoRequest/SignUpRequestDTO.java
@@ -1,7 +1,6 @@
 package org.example.e_commerce.dto.dtoRequest;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,20 +9,33 @@ import lombok.Setter;
 
 public class SignUpRequestDTO {
 
+    @NotNull(message = "First name is mandatory")
     @NotBlank(message = "First name is mandatory")
     private String firstname;
 
+    @NotNull(message = "Last name is mandatory")
     @NotBlank(message = "Last name is mandatory")
     private String lastname;
 
+    @NotNull(message = "Username is mandatory")
     @NotBlank(message = "Username is mandatory")
     private String username;
 
+    @NotNull(message = "Email is mandatory")
     @NotBlank(message = "Email is mandatory")
-    @Email(message = "Email should be valid")
+    @Pattern(
+            regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z]{2,6}$",
+            message = "message: Email should be valid"
+    )
     private String email;
 
+    @NotNull(message = "Password is mandatory")
     @NotBlank(message = "Password is mandatory")
+    @Size(min = 6, message = "Password must have at least 6 characters")
+    @Pattern(
+            regexp = "^(?=.*[A-Z])(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?]).+$",
+            message = "Password must contain at least one uppercase letter and one special character"
+    )
     private String password;
 
     public void setFirstname(String firstname) {
