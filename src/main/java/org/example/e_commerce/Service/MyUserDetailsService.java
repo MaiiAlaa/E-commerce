820a38df71fd7860d99b0114c116af6fd8bc5975
kareem <kareemismail259@gmail.com>
2024-08-13 20:39:49 +0300

    last el last fixing

--- src/main/java/org/example/e_commerce/Service/MyUserDetailsService.java
+++ src/main/java/org/example/e_commerce/Service/MyUserDetailsService.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Collections;
 
 @Service
@@ -18,15 +19,9 @@ public class MyUserDetailsService implements UserDetailsService {
     private UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        // Ensure the findByUsername method returns an Optional<User>
-        User userEntity = userRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
-
-        return new org.springframework.security.core.userdetails.User(
-                userEntity.getUsername(),
-                userEntity.getPasswordHash(),
-                Collections.singletonList(new SimpleGrantedAuthority(userEntity.getRole()))
-        );
+    public UserDetails loadUserByUsername (String username) throws UsernameNotFoundException
+    {
+        User user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPasswordHash(), new ArrayList<>());
     }
 }
