6444f302b35b3bbef55be74aea86ddd684472733
kareem <kareemismail259@gmail.com>
2024-08-15 17:15:47 +0300

    Product Images Added

--- src/main/java/org/example/e_commerce/dto/dtoRequest/ProductRequestDTO.java
+++ src/main/java/org/example/e_commerce/dto/dtoRequest/ProductRequestDTO.java
@@ -1,21 +1,45 @@
 package org.example.e_commerce.dto.dtoRequest;
 
 import lombok.Data;
-import org.example.e_commerce.Entity.Product;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
 public class ProductRequestDTO {
-    // vilidation hena mydakhlsh haga b null
-    List<Product> productList = new ArrayList<>();
+
+    @NotNull(message = "Product ID is mandatory")
     private Long productID;
+
+    @NotBlank(message = "Product name is mandatory")
     private String productName;
-    private Long categoryID; // Reference to Category table
+
+    @NotNull(message = "Category ID mandatory")
+    private Long categoryID;
+
+    @NotNull(message = "Price is mandatory")
+    @Positive(message = "Price must be positive")
     private Double price;
+
+    @NotNull(message = "Stock quantity is mandatory")
+    @Positive(message = "Stock quantity must be positive")
     private Integer stockQuantity;
+
+    @NotBlank(message = "Description is mandatory")
     private String description;
+
+    @NotNull(message = "Warranty period is mandatory")
+    @Positive(message = "Warranty period must be positive")
     private Integer warrantyPeriod;
+
+    @NotBlank(message = "Manufacturer is mandatory")
     private String manufacturer;
+
+    @NotBlank(message = "Main image URL is mandatory")
+    private String mainImageUrl;
+
+    private List<String> imageUrls = new ArrayList<>();
 }
