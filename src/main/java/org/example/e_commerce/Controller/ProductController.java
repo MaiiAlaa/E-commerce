2b0e723bf86212221e3ae9c81b2d8403294443b9
Mahmoud <mahmoudali77009@gmail.com>
2024-08-15 14:08:38 +0300

    update/add/search/product and getproductbycategoryId

--- src/main/java/org/example/e_commerce/Controller/ProductController.java
+++ src/main/java/org/example/e_commerce/Controller/ProductController.java
@@ -1,12 +1,17 @@
 package org.example.e_commerce.Controller;
 
+import jakarta.validation.Valid;
+import org.example.e_commerce.Entity.Product;
 import org.example.e_commerce.Service.ProductService;
+import org.example.e_commerce.dto.dtoRequest.ProductRequestDTO;
 import org.example.e_commerce.dto.dtoResponse.ProductResponseDTO;
+import org.example.e_commerce.dto.dtoResponse.SignUpResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
@@ -22,5 +27,39 @@ public class ProductController {
     @GetMapping("/{id}")
     public ProductResponseDTO getProductById(@PathVariable Long id) {
         return productService.getProductById(id);
+    }
+
+    @PostMapping("/update")
+    public ResponseEntity<SignUpResponseDTO> updateProduct(@Valid @RequestBody ProductRequestDTO productDTO) {
+
+        SignUpResponseDTO response = productService.updateProduct(productDTO);
+
+        if (response.getStatusCode() != 0l) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        }
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/by_category/{categoryId}")
+    public ResponseEntity<List<Product>> findProductCategoryId(@PathVariable Long categoryId) {
+        List<Product> products = productService.findProductCategoryId(categoryId);
+        if (products.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(products);
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<Product>> searchProducts(@RequestParam String search) {
+        List<Product> products = productService.searchProducts(search);
+        if (products.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(products);
     }
 }
