56c88ba79cf822541d23377c361a634c81862af8
Mahmoud <mahmoudali77009@gmail.com>
2024-08-19 15:20:39 +0300

    feature/get-transactions

--- src/main/java/org/example/e_commerce/Repository/TransactionRepo.java
+++ src/main/java/org/example/e_commerce/Repository/TransactionRepo.java
@@ -3,6 +3,9 @@ package org.example.e_commerce.Repository;
 import org.example.e_commerce.Entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TransactionRepo extends JpaRepository<Transaction, Long> {
 
+    List<Transaction> getAllTransactions();
 }
