6dcef8f778dd410292fb6a6780e7cfa9c8424735
AdhamAkram <adham.akraam@gmail.com>
2024-09-09 14:57:11 +0300

    products imgaes

--- src/main/java/org/example/e_commerce/config/WebConfig.java
+++ src/main/java/org/example/e_commerce/config/WebConfig.java
@@ -1,6 +1,7 @@
 package org.example.e_commerce.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,5 +12,12 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:uploads/");
+    }
+
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")  // Adjust this to your allowed origins
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
     }
 }
