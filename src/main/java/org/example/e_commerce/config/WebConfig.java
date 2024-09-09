ac1e74e6632ca094768292aa1156ebab875d570a
AdhamAkram <adham.akraam@gmail.com>
2024-09-09 14:58:26 +0300

    products imgaes

--- src/main/java/org/example/e_commerce/config/WebConfig.java
+++ src/main/java/org/example/e_commerce/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:uploads/");
     }
-
+    @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")  // Adjust this to your allowed origins
